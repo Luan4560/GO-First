b784e1a1325b02617dd72236bb226faeb25463ab
unknown <luan.nascimento11@outlook.com>
2019-10-29 16:56:18 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./pages/Header/Header";
+import Post from "./pages/Post/Post";
+ReactDOM.render([<Header />, <Post />], document.getElementById("app"));
---
+++ src/pages/Header/Header.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export default class Header extends Component {
+  render() {
+    return <h1 className="title">RocketBook</h1>;
+  }
+}
